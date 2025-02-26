af2af5727a4d3b28b02ece9938f7ae8c5a0dc304
Giancarlo Mennillo <giancarlo.mennillo@safectory.com>
2025-02-26 19:41:34 +0100

    add new button use-case

--- widgetbook/lib/ui/widgets/primary_button.dart
+++ widgetbook/lib/ui/widgets/primary_button.dart
@@ -6,5 +6,19 @@ import 'package:groceries_app/ui/ui.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart';
 
+@UseCase(
+  name: 'Default',
+  type: PrimaryButton
+)
+Widget buildPrimaryButton(BuildContext context) {
+  return PrimaryButton(content:
+    context.knobs.string(
+      label: 'Content',
+      initialValue: 'Button',
+    ),
+
+  );
+}
+
 // TODO: Implement the PrimaryButton use-case
 // DESIGN: https://www.figma.com/design/HsANkdhbsCNTkXBzNJRNLD/Groceries-Demo?node-id=7235-4663&t=N6qwmLP7MP59ClWB-4
